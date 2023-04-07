39febb93a9bb7b22f1ee5fa3d9212f6fd065ac57
Whyzen <girard.paul39@gmail.com>
2023-04-08 00:13:49 +0200

    [Add] init projecr

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+  <section>
+    <h1 class="title">Github <span>Jobs</span></h1>
+  </section>
+</template>
+
+<style scoped>
+section {
+  padding: 3.2rem 12rem;
+}
+.title {
+  font-size: 2.4rem;
+  font-weight: 700;
+  color: #282538;
+}
+.title span {
+  font-size: 2.4rem;
+  font-weight: 300;
+  color: #282538;
+}
+</style>
